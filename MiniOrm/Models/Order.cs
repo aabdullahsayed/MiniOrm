661cf030cc26e7f6e6fe9ea406a9eda78ca95823
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-05-10 20:12:46 +0600

    feat: define domain models for ORM implementation
    - Add Product and Order entity classes
    - Apply custom attributes ([Table], [Column], [PrimaryKey]) to link C# models to PostgreSQL tables

---
+++ MiniOrm/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using MiniOrm.Attributes;
+namespace MiniOrm.Models;
+[Table("Orders")]
+public class Order
+{
+    [PrimaryKey]
+    public int ID { get; set;}
+    
+    [Column("ProductName")]
+    public string Pname { get; set; }
+}
---
+++ MiniOrm/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using MiniOrm.Attributes;
+
+[Table("products")]
+public class Product {
+    [PrimaryKey] public int Id { get; set; }
+    [Column("name")] public string Name { get; set; }
+    [Column("price")] public decimal Price { get; set; }
+    [Column("discount")] public decimal? Discount { get; set; } // nullable
+    [Column("in_stock")] public bool InStock { get; set; }
+}
