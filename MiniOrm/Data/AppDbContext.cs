d7fbbda5d702cff2b33109acd9c49c67abdab068
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-05-10 19:56:28 +0600

    feat: setup database context infrastructure
    - Add abstract DbContext for connection management and DbSet initialization
    - Add AppDbContext with DbSets

---
+++ MiniOrm/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using MiniOrm.Data;
+using MiniOrm.Models;
+
+namespace MiniOrm;
+
+/*
+zero logic here we simply list the tables we want (DbSet<Product>, DbSet<Order>) and pass the connection string to the engine
+*/
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    public AppDbContext(string connStr) : base(connStr)
+    {
+     
+    }
+}
---
+++ MiniOrm/Data/DbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Npgsql;
+
+namespace MiniOrm.Data;
+/*
+holds the actual database connection and uses reflection to automatically build  tables 
+ */
+public abstract class DbContext : IDisposable
+{
+
+    internal NpgsqlConnection Connection { get; }
+
+    protected DbContext(string connectionString)
+    {
+        if (string.IsNullOrWhiteSpace(connectionString))
+        {
+            throw new ArgumentException("Connection string is missing!", nameof(connectionString));
+        }
+        
+        Connection = new NpgsqlConnection(connectionString);
+        Connection.Open();
+        
+        InitializeDbSets();
+    }
+
+    private void InitializeDbSets()
+    {
+        
+        var properties = this.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Where(p => p.PropertyType.IsGenericType && 
+                        p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>));
+
+        
+        foreach (var prop in properties)
+        {
+            var dbSetInstance = Activator.CreateInstance(prop.PropertyType, this);
+            prop.SetValue(this, dbSetInstance);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (Connection != null)
+        {
+            if (Connection.State == System.Data.ConnectionState.Open)
+            {
+                Connection.Close();
+            }
+            Connection.Dispose();
+        }
+    }
+}
